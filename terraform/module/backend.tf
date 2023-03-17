015dea2ae40a1790920ef6f0b926687211ba62fa
Zef Delgadillo <zef@google.com>
2023-03-17 01:45:15 -0400

    Add Cloud Build

---
+++ terraform/module/backend.tf
@@ -0,0 +1,5 @@
+terraform {
+  backend "gcs" {
+    bucket = "firewall-delegation-f9tn"
+  }
+}
--- terraform/module/main.tf
+++ terraform/module/main.tf
@@ -1,8 +1,9 @@
 locals {
+  team             = terraform.workspace
   rules_map        = yamldecode(var.yaml_content)
   team_config_map  = yamldecode(var.team_config_content)
-  priority_start   = lookup(local.team_config_map, local.rules_map.team)[0]
-  priority_end     = lookup(local.team_config_map, local.rules_map.team)[1]
+  priority_start   = lookup(local.team_config_map, local.team)[0]
+  priority_end     = lookup(local.team_config_map, local.team)[1]
   default_priority = local.priority_end
 }
 
@@ -14,7 +15,7 @@ resource "google_compute_firewall" "firewall_rule" {
   for_each = { for rule in local.rules_map.rules : rule.name => rule }
   project  = var.project_id
   network  = var.vpc_network_name
-  name     = each.value.name
+  name     = "${local.team}-${each.value.name}"
   dynamic "allow" {
     for_each = each.value.action == "allow" ? [1] : []
     content {
